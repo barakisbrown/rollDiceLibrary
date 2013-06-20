aa7be8c8d277a2b9e524d9e2d79074170da9b4e3
barakis <barakis@barakisbrown.com>
2013-06-20 17:58:33 -0500

    added the following functions:
    int getTotal()
    iterator<Die> getDice()
    String getDiceString()

--- rolldice/src/com/barakisbrown/rolldice/DicePool.java
+++ rolldice/src/com/barakisbrown/rolldice/DicePool.java
@@ -1,22 +1,27 @@
 package com.barakisbrown.rolldice;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class DicePool 
 {
 	private ArrayList<Die> pool;
 	private int numDice;
+	private int total;
+	private int timesRolled;
 	
 	public DicePool()
 	{
 		pool = new ArrayList<Die>();
 		numDice = 1;
+		total = 0;
 	}
 	
 	public DicePool(int number,int sides)
 	{
 		numDice = number;
 		pool = new ArrayList<Die>();
+		total = 0;
 		
 		try
 		{
@@ -35,23 +40,44 @@ public class DicePool
 	{
 		try
 		{
-			
-			pool.add(new Die(number,sides));
+			Die newDie = new Die(number,sides);
+			pool.add(newDie);
 			
 		}catch(Exception e)
 		{
 			System.err.println(e.getMessage());
 		}
+
+		if (numDice != 1)
+			numDice++;
+
 	}
 	
 	public void generate()
 	{
-
 		for (Die dice : pool)
 		{
 			dice.rollDie();
+			total += dice.getvalue();
 		}
-			
+		timesRolled++;
 	}
 	
+	public int getTotal()
+	{
+		if (timesRolled == 0)
+			return 0;
+		else
+			return total;
+	}
+	
+	public Iterator<Die> getDice()
+	{
+		return pool.iterator();
+	}
+	
+	public String getDiceString()
+	{
+		return numDice + pool.get(0).getDie();
+	}
 }
