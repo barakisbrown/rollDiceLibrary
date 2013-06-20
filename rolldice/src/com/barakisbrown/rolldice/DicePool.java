f0b5607407b9c2c9bb14f60049d32aa4eaca6cb3
barakis <barakis@barakisbrown.com>
2013-06-20 17:28:50 -0500

    Class representing dice in your hand. It can be a single one or it can be mulitple.

---
+++ rolldice/src/com/barakisbrown/rolldice/DicePool.java
@@ -0,0 +1,46 @@
+package com.barakisbrown.rolldice;
+
+import java.util.ArrayList;
+
+public class DicePool 
+{
+	private ArrayList<Die> pool;
+	private int numDice;
+	
+	public DicePool()
+	{
+		pool = new ArrayList<Die>();
+		numDice = 1;
+	}
+	
+	public DicePool(int number,int sides)
+	{
+		numDice = number;
+		pool = new ArrayList<Die>();
+		
+		try
+		{
+			for (int loop = 0;loop < numDice;loop++)
+			{
+				pool.add(new Die(number,sides));			
+			}
+		}
+		catch(Exception e)
+		{
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	public void addDice(int number,int sides)
+	{
+		try
+		{
+			
+			pool.add(new Die(number,sides));
+			
+		}catch(Exception e)
+		{
+			System.err.println(e.getMessage());
+		}
+	}
+}
