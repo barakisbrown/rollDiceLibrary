3f42d322a571e280f10b2e7e2c5d8e6215c34de2
Matthew Brown <barakis@barakisbrown.com>
2013-07-05 20:05:04 -0500

    now I have added exploding dice support.

---
+++ rolldice/src/com/barakisbrown/rolldice/ExplodedDie.java
@@ -0,0 +1,44 @@
+package com.barakisbrown.rolldice;
+
+public class ExplodedDie extends Die 
+{
+	private boolean exploded;
+	private int explode_target_number;
+
+	public ExplodedDie(int value, int side) throws Exception {
+		super(value, side);
+		// TODO Auto-generated constructor stub
+		exploded = false;
+		explode_target_number = 0;
+	}
+
+	public void setExplodeTN(int targetNumber)
+	{
+		if ((targetNumber < 1) || (targetNumber > getSide()))
+				return;
+		else
+			explode_target_number = targetNumber;
+	}
+	
+	public boolean didExplode()
+	{
+		return exploded;
+	}
+	
+	public void rollDie() throws Exception
+	{
+		super.rollDie();
+		
+		if (explode_target_number == 0)
+			throw new Exception("Exploded not defined");
+		else
+		{
+			int value = getvalue();
+			if (value == explode_target_number)
+				exploded = true;
+		}
+		
+	}
+	
+}
+
