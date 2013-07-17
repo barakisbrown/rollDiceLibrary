b0b5807b2653302a4eec2e321c74a067f1c5aa27
barakis <barakis@barakisbrown.com>
2013-07-17 15:24:44 -0500

    Adding Files that will constitute my library for the application.

---
+++ RollDice/src/library/DiceTypes.java
@@ -0,0 +1,23 @@
+package library;
+
+
+/***
+ * DiceTypes will be which dice that the library and eventually the application will be allowed to generate.
+ * They will cover the basics [D3/D4/D6/D8/D10/D12/D20/D100] that could be used in any type of roll playing game.
+ * @author barakis
+ *
+ */
+public class DiceTypes 
+{
+	private static final int []allowedDice = {3,4,6,8,10,12,20,100};
+	
+	public static boolean isAllowed(int dice)
+	{
+		for (int loop=0;loop<allowedDice.length;loop++)
+		{
+			if (dice == allowedDice[loop])
+				return true;
+		}
+		return false;
+	}
+}
