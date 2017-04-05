e529603f3dcaa97bd19137f53fb540ba9c02aa04
Matthew Brown <lokislayer@gmail.com>
2017-04-05 05:59:19 -0500

    Roll(X,DiceTypes) Added
    I have now competely tested Roll with two arguments. 
    X = number of dice rolled
    DiceTypes  = which dice to roll

--- RollDice/RollDiceLibrary/RollDice.cs
+++ RollDice/RollDiceLibrary/RollDice.cs
@@ -30,5 +30,18 @@ namespace RollDiceLibrary
 			var n = rand.Next(1, rolled);
 			return n;
 		}
+
+	    public static int Roll(int amount, DiceUtilities.Dice_Types D)
+	    {
+	        if (amount < 1)
+                throw new ArgumentException("Amount must be greater than or equal to 1. Can not roll 0 Dice");
+	        else
+	        {
+	            var total = 0;
+	            for (var Loop = 0; Loop < amount; Loop++)
+	                total += Roll(D);
+	            return total;
+	        }
+	    }
 	}
 }
